c6b66e0bf461daccf5faccf6117c63ee4b10f0ac
extrausa <d.kirenkov@gmail.com>
2021-01-24 13:03:05 +0300

    hello

---
+++ src/test/java/ru/job4j/TriggerTest.java
@@ -0,0 +1,17 @@
+package ru.job4j;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class TriggerTest extends TestCase {
+    @Test
+    public void test() {
+        Assert.assertEquals(1, new Trigger().someLogic());
+    }
+
+
+
+}
