098d52c3c1f420fc57486c216375a906087dd823
extrausa <d.kirenkov@gmail.com>
2021-04-27 14:39:54 +0300

    Изменнение вынешнего вида

---
+++ src/main/java/ru/job4j/hashMap/UserApp.java
@@ -0,0 +1,28 @@
+package ru.job4j.hashMap;
+
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
+
+public class UserApp {
+    public static void main(String[] args) {
+
+        User one = new User("Ivan", 4, (
+                new GregorianCalendar(2012, 12, 12)));
+        User two = new User("Ivan", 4, (
+                new GregorianCalendar(2012, 12, 12)));
+
+        Map<User, Object> userObjectMap = new HashMap<>();
+        userObjectMap.put(one, new  Object());
+        userObjectMap.put(two, new  Object());
+
+        userObjectMap.entrySet().forEach(
+                userObjectEntry -> System.out.println(userObjectEntry.getKey() + " " + userObjectEntry.getValue()));
+
+
+    }
+}
+
+
+
+
