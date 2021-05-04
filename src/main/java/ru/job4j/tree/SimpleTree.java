1f705194a920f67f27a05db4adebce1fea9965fa
extrausa <d.kirenkov@gmail.com>
2021-05-04 17:13:07 +0300

    1. Создать элементарную структуру дерева [#455152]

---
+++ src/main/java/ru/job4j/tree/SimpleTree.java
@@ -0,0 +1,50 @@
+package ru.job4j.tree;
+//1. Создать элементарную структуру дерева [#455152]
+//import java.util.LinkedList;
+//import java.util.Optional;
+//import java.util.Queue;
+import java.util.*;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class SimpleTree<E> implements Tree<E> {
+    private final Node<E> root;
+
+    public SimpleTree(final E root) {
+        this.root = new Node<>(root);
+    }
+
+    @Override
+    public boolean add(E parent, E child) {
+        boolean rsl = false;
+        Node<E> chTest = new Node<>(child);
+        Node<E> pareTest  = new Node<>(parent);
+        if (findBy(parent).isPresent()) {
+            root.children.add(chTest);
+            rsl  = true;
+        } else {
+            root.children.add(pareTest);
+            if (findBy(parent).isPresent()) {
+                root.children.add(chTest);
+                rsl = true;
+            }
+        }
+        return rsl;
+    }
+
+    @Override
+    public Optional<Node<E>> findBy(E value) {
+        Optional<Node<E>> rsl = Optional.empty(); //возвращает пустой экземпляр
+        Queue<Node<E>> data = new LinkedList<>(); // очередь на связном списке
+        data.offer(this.root); //добавляем Node в очередь
+        while (!data.isEmpty()) { // выполняется пока не пустая
+            Node<E> el = data.poll(); // резервная Noda со значением из головы очереди, удаляя его
+            if (el.value.equals(value)) { // если в резервной ноде значение равно значению передаваемом в методе
+                rsl = Optional.of(el); // оптионал снабжается не нулевым значением
+                break; // объект найден стоп
+            }
+            data.addAll(el.children); // в очередь возвращается значение и помещается в массив с сылкой на ребенока
+        }
+        return rsl;
+    }
+}
