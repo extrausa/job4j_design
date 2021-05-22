3d9fad91565d2466efc4d2aeb986fa3b6d7e4da1
Denis <d.kirenkov@gmail.com>
2021-05-22 08:42:13 +0300

    Почта. [#184458]

---
+++ src/main/java/ru/job4j/map/Model.java
@@ -0,0 +1,18 @@
+package ru.job4j.map;
+
+import ru.job4j.simpleArray.Set;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Model {
+    private Map<User, Email> table = new HashMap<>();
+
+    public boolean add(User user, Email email){
+        boolean rsl = false;
+        for (Email e : table.
+             ) {
+
+        }
+    }
+}
