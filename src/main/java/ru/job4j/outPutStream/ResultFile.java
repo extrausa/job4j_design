8f04fc8c644c22cb24adcb679555e06d52cff371
Denis <d.kirenkov@gmail.com>
2021-06-19 07:43:39 +0300

    //0.1. FileOutputStream. [#252488]

--- src/main/java/ru/job4j/outPutStream/ResultFile.java
+++ src/main/java/ru/job4j/outPutStream/ResultFile.java
@@ -7,22 +7,19 @@ import java.nio.charset.StandardCharsets;
 
 public class ResultFile {
     public static void main(String[] args) throws IOException {
-        FileOutputStream fo = new FileOutputStream("result.txt");
-        try {
 
+        try (FileOutputStream fo = new FileOutputStream("result.txt")){
             for (int i = 1; i < 10; i++) {
                 for (int j = 1; j < 10; j++) {
                     fo.write(Integer.toString(i * j).getBytes(StandardCharsets.UTF_8));
                     fo.write(" * ".getBytes(StandardCharsets.UTF_8));
                 }
-                fo.write("\n".getBytes(StandardCharsets.UTF_8));
-
+                fo.write(System.lineSeparator().getBytes(StandardCharsets.UTF_8));
             }
             fo.close();
             System.out.println("Information recorded");
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 }
