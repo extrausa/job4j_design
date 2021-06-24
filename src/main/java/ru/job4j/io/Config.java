f80b3214a9bae5fcdbbfda42ca53ac90d469b41c
extrausa <d.kirenkov@gmail.com>
2021-06-24 17:41:31 +0300

    1. Читаем файл конфигурации [#858]

---
+++ src/main/java/ru/job4j/io/Config.java
@@ -0,0 +1,56 @@
+package ru.job4j.io;
+//1. Читаем файл конфигурации [#858]
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringJoiner;
+
+public class Config {
+
+    private final String path; //"./data/pair_without_comment.properties;
+    private final Map<String, String> values = new HashMap<String, String>();
+
+
+    public Config(final String path) {
+        this.path = path;
+    }
+
+    public void load() {
+        String[] substr;
+        try (BufferedReader in = new BufferedReader(new FileReader(path))) {
+            for (String line = in.readLine(); line != null; line = in.readLine()) {
+                substr = line.split("=");
+                for (int i = 0; i < substr.length; i++) {
+                    if (substr.length >= 2) {
+                        values.put(substr[0], substr[1]);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String value(String key) {
+        for (String s : values.values()) {
+            if (s.contains(key)) {
+                return s;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        StringJoiner out = new StringJoiner(System.lineSeparator());
+        try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
+            read.lines().forEach(out::add);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return out.toString();
+    }
+
+
+}
---
+++ src/test/java/ru/job4j/io/ConfigTest.java
@@ -0,0 +1,19 @@
+package ru.job4j.io;
+
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import static org.junit.Assert.assertThat;
+
+public class ConfigTest {
+
+    @Test
+    public void whenPairWithoutComment() {
+        String path = "./data/app.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("hibernate.dialect"),is("org.hibernate.dialect.PostgreSQLDialect"));
+    }
+
+}
