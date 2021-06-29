453686344baa41b3519ca6e68ca72d3e0696015f
extrausa <d.kirenkov@gmail.com>
2021-06-29 16:54:15 +0300

    2. Анализ доступности сервера. [#859]

--- src/main/java/ru/job4j/io/Analizy.java
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -7,48 +7,44 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class Analizy {
+    @SuppressWarnings("checkstyle:WhitespaceAround")
     public void unavailable(String source, String target) {
         List<String> separated = new ArrayList<>();
         List<String> queue = new LinkedList<>();
-
-        int badRequest = 400;
-        int internalServerError = 500;
-        boolean point = false;
-//        source = "./server.log";
-//        target = "./unavailable.csv";
+        int count = 0;
         try (BufferedReader in = new BufferedReader(new FileReader(source))) {
             for (String line = in.readLine(); line != null; line = in.readLine()) {
                 separated = Arrays.asList(line.split(" "));
-                if (Integer.parseInt(separated.get(0)) == badRequest || Integer.parseInt(separated.get(0)) == internalServerError) {
+                if ((line.startsWith("400") || line.startsWith("500"))) {
+                    if (count == 1) {
+                        continue;
+                    } else {
+                        queue.add(separated.get(1));
+                        count++;
+                    }
+                } else if (count > 0) {
                     queue.add(separated.get(1));
-                }else {
+                    count = 0;
+                } else {
                     continue;
                 }
             }
             if (queue.size() != 0) {
                 try (PrintWriter out = new PrintWriter(new FileOutputStream(target))) {
-
-                    out.println(queue.get(0));
-                    out.println(queue.get(queue.size() - 1));
+                    for (int i = 0; i < queue.size() - 1; i++) {
+                        out.println(queue.get(i) + " " + queue.get(i + 1));
+                        i++;
+                    }
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
-
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-
     }
 
     public static void main(String[] args) {
-//        try (PrintWriter out = new PrintWriter(new FileOutputStream("unavailable.csv"))) {
-//            out.println("15:01:30;15:02:32");
-//            out.println("15:10:30;23:12:32");
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
         String source = "./server.log";
         String target = "./unavailable.csv";
         Analizy first = new Analizy();
