0b94e12fb07004d1bbe304ac626e3fa3a5189814
extrausa <d.kirenkov@gmail.com>
2021-07-01 17:32:56 +0300

    4.0. File [#252491]

---
+++ src/main/java/ru/job4j/io/Dir.java
@@ -0,0 +1,21 @@
+package ru.job4j.io;
+//4.0. File [#252491]
+import java.io.File;
+
+public class Dir {
+    public static void main(String[] args) {
+        File file = new File("/home/extrausa/IdeaProjects");
+        if (!file.exists()) {
+            throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
+        }
+        if (!file.isDirectory()) {
+            throw new IllegalArgumentException(String.format("Not directory %s", file.getAbsoluteFile()));
+        }
+        System.out.println(String.format("size : %s", file.getTotalSpace()));
+        for (File subfile : file.listFiles()) {
+            //System.out.println(subfile.getAbsoluteFile());
+            System.out.println("Name derictory " + subfile.getName() + " " + " Amount of elements " + subfile.getName().length());
+        }
+    }
+
+}
