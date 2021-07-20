c65cb09806d0459199865e7919af5e9567632962
extrausa <d.kirenkov@gmail.com>
2021-07-20 15:41:12 +0300

    4.2. Поиск дубликатов [#315066 #177601]

--- src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -1,19 +1,31 @@
 package ru.job4j.io.duplicates;
 
 import java.io.IOException;
-import java.lang.reflect.ParameterizedType;
 import java.nio.file.FileVisitResult;
-import java.nio.file.FileVisitor;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
+    FileProperty fileProp;
+    HashMap<Long, String> map = new HashMap<>();
+    List<String> arrayList = new ArrayList<>();
 
     @Override
     public FileVisitResult visitFile(Path path, BasicFileAttributes attrs) throws IOException {
-        System.out.println(path.toAbsolutePath());
+        fileProp = new FileProperty(path.toFile().length(), path.toFile().getName());
+
+        if (map.size() != 0 && map.containsKey(fileProp.getSize()) && map.containsValue(fileProp.getName())) {
+            arrayList.add(fileProp.getName());
+        }
+        map.put(fileProp.getSize(), fileProp.getName());
         return super.visitFile(path, attrs);
     }
 
+    public List<String> getArrayList() {
+        return arrayList;
+    }
 }
