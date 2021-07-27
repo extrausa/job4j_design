4f8c3a35fbd44a861aa05951fc399fda3f01b929
extrausa <d.kirenkov@gmail.com>
2021-07-27 14:56:21 +0300

    //5.1. Именованные аргументы [#295518]

--- src/main/java/ru/job4j/io/ArgsName.java
+++ src/main/java/ru/job4j/io/ArgsName.java
@@ -17,18 +17,13 @@ public class ArgsName {
             throw new IllegalArgumentException();
         }
         for (int i = 0; i < args.length; i++) {
-            if (args[i].startsWith("-Xmx")) {
-                int a = args[i].length();
-                if (args[i].length() <= 5) {
-                    throw new IllegalArgumentException();
-                }
-                String line = args[i];
-                int iend = line.indexOf("=");
-                values.put(line.substring(1, iend), line.substring(++iend, line.length()));
-                break;
-            } else {
-                continue;
+            int a = args[i].length();
+            String line = args[i];
+            int iend = line.indexOf("=");
+            if (line.substring(++iend, line.length()).length() <= 0) {
+                throw new IllegalArgumentException();
             }
+            values.put(line.substring(1, --iend), line.substring(++iend, line.length()));
         }
     }
 
