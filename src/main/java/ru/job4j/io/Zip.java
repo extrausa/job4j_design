f1af3d11874840f653f7156e21333a16804056a0
Denis <d.kirenkov@gmail.com>
2021-07-29 05:48:25 +0300

    5.2. Архивировать проект [#861]

--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -1,12 +1,14 @@
 package ru.job4j.io;
-
+//5.2. Архивировать проект [#861]
 import java.io.*;
+import java.nio.file.Path;
 import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 public class Zip {
-    public static void packFiles(List<File> sources, File target) {
+    public static void packFiles(List<Path> sources, File target) {
+
 
 
     }
@@ -23,16 +25,15 @@ public class Zip {
     }
 
     public static void main(String[] args) {
+//        packSingleFile(
+//                new File("/home/extrausa/IdeaProjects/job4j_design/test/pom.xml"),
+//                new File("/home/extrausa/IdeaProjects/job4j_design/test/pom.zip")
+//        );
+        //String filename = "/home/extrausa/IdeaProjects/job4j_design/test";
+        String filename =  "/home/denis/IdeaProjects/job4j_design/test";
+        Search searchFile = new Search();
 
-        String filename = "/home/extrausa/IdeaProjects/job4j_design/test";
-//        if (args.length == 0) {
-//            throw new IllegalArgumentException("Root folder is null. Usage java -jar dir.jar ROOT_FOLDER.");
-//        }
-//        File file = new File(args[0]);
-        packSingleFile(
-                new File("/home/extrausa/IdeaProjects/job4j_design/test/pom.xml"),
-                new File("/home/extrausa/IdeaProjects/job4j_design/test/pom.zip")
-        );
+
         //packFiles();
     }
 }
