c1cae219c5a5749ef9f7fc375ee6993a5530d3b4
Denis <d.kirenkov@gmail.com>
2021-08-13 08:06:35 +0300

    7. Scanner [#504791]

--- src/main/java/ru/job4j/io/CSVReader.java
+++ src/main/java/ru/job4j/io/CSVReader.java
@@ -10,7 +10,7 @@ import java.util.*;
 
 public class CSVReader {
     public List<String> list = new ArrayList<>();
-    public Map<String, Integer> map = new HashMap<>();
+    public Map<Integer, String> map = new HashMap<>();
 
     public List<String> reader(Path path, Charset cs, String[] filter) throws FileNotFoundException {
         String nameUser = "name";
@@ -21,25 +21,25 @@ public class CSVReader {
         }
         for (int i = 0; i < list.size(); i++) {
             int count = 0;
-            if (count == 0) {
-                Scanner words = new Scanner(new ByteArrayInputStream(list.get(i).getBytes(StandardCharsets.UTF_8))).useDelimiter(", ");
-                for (Scanner it = words; it.hasNext();) {
-                    String a = it.next();
-                    map.put(a, count++);
-                }
-                count = 0;
+            Scanner words = new Scanner(new ByteArrayInputStream(list.get(i).getBytes(StandardCharsets.UTF_8))).useDelimiter(", ");
+            for (Scanner it = words; it.hasNext();) {
+                String a = it.next();
+                map.put(count++, a);
             }
         }
         List<String> templist = new ArrayList<>();
         int countWhile = 0;
-        while (filter.length < countWhile) {
-            for (int i = 0; i < map.size(); i++) {
-                String namePoint = filter[countWhile];
-                int keyTemp = map.get(namePoint).intValue();
-                if (map.containsValue(keyTemp)) {
-
-                    templist.add(map.get(namePoint).toString());
+        while (filter.length > countWhile) {
+            String namePoint = filter[countWhile];
+            int keyTemp = 0;
+            for (Integer key : map.keySet()) {
+                if (map.get(key).toString().equals(namePoint)) {
+                    keyTemp = key;
+                    break;
                 }
+            }
+            for (int i = 0; i < map.size(); i++) {
+
             }
             countWhile++;
         }
@@ -47,8 +47,8 @@ public class CSVReader {
     }
 
     public static void main(String[] args) throws FileNotFoundException {
-        Path start = Paths.get("/home/extrausa/IdeaProjects/job4j_design/test/table.csv");
-        //Path start = Paths.get("/home/denis/IdeaProjects/job4j_design/test/table.csv");
+        //Path start = Paths.get("/home/extrausa/IdeaProjects/job4j_design/test/table.csv");
+        Path start = Paths.get("/home/denis/IdeaProjects/job4j_design/test/table.csv");
         CSVReader reader = new CSVReader();
         String[] name = new String[]{"name", "age"};
         for (String s : reader.reader(start, StandardCharsets.UTF_8, name)) {
