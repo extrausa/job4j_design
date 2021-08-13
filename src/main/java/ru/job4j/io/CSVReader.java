325a944cc639099e8a12192f73eb48a97dedac34
extrausa <d.kirenkov@gmail.com>
2021-08-13 07:11:52 +0300

    6. Кодировка. [#862]

--- src/main/java/ru/job4j/io/CSVReader.java
+++ src/main/java/ru/job4j/io/CSVReader.java
@@ -23,27 +23,36 @@ public class CSVReader {
             int count = 0;
             if (count == 0) {
                 Scanner words = new Scanner(new ByteArrayInputStream(list.get(i).getBytes(StandardCharsets.UTF_8))).useDelimiter(", ");
-                for (Scanner it = words; it.hasNext(); ) {
+                for (Scanner it = words; it.hasNext();) {
                     String a = it.next();
-                    map.put(a,count++);
+                    map.put(a, count++);
                 }
                 count = 0;
             }
         }
-        for (int i = 0; i < filter.length; i++) {
-            map
-        }
+        List<String> templist = new ArrayList<>();
+        int countWhile = 0;
+        while (filter.length < countWhile) {
+            for (int i = 0; i < map.size(); i++) {
+                String namePoint = filter[countWhile];
+                int keyTemp = map.get(namePoint).intValue();
+                if (map.containsValue(keyTemp)) {
 
+                    templist.add(map.get(namePoint).toString());
+                }
+            }
+            countWhile++;
+        }
         return list;
     }
 
     public static void main(String[] args) throws FileNotFoundException {
-        Path start = Paths.get("/home/denis/IdeaProjects/job4j_design/test/table.csv");
+        Path start = Paths.get("/home/extrausa/IdeaProjects/job4j_design/test/table.csv");
+        //Path start = Paths.get("/home/denis/IdeaProjects/job4j_design/test/table.csv");
         CSVReader reader = new CSVReader();
-        for (String s : reader.reader(start, StandardCharsets.UTF_8)) {
+        String[] name = new String[]{"name", "age"};
+        for (String s : reader.reader(start, StandardCharsets.UTF_8, name)) {
             System.out.println(s);
         }
-
     }
-    
 }
