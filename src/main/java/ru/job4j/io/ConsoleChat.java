b52b277812990425eafa37513b6fdae86a4048ef
Denis <d.kirenkov@gmail.com>
2021-08-15 06:00:14 +0300

    //6. Кодировка. [#862]

--- src/main/java/ru/job4j/io/ConsoleChat.java
+++ src/main/java/ru/job4j/io/ConsoleChat.java
@@ -13,6 +13,7 @@ public class ConsoleChat {
     private static final String CONTINUE = "продолжить";
     private List<String> dataTemp = new ArrayList<>();
     boolean checkFlag = true;
+    boolean fileinput = true;
     List<String> words;
     int count = 0;
 
@@ -24,81 +25,87 @@ public class ConsoleChat {
 
     public String run(String word) throws IOException {
         String answer = word;
-        if (count == words.size()) {
-            count = 0;
-        }
-        if (word.equals(CONTINUE)) {
-            checkFlag = true;
-            dataTemp.add(word);
-            answer = words.get(count);
-            dataTemp.add(answer);
-            count++;
-            return answer;
-        }
-        if (word.equals(OUT)) {
-            dataTemp.add(OUT);
-            writeDataInFile(botAnswers, dataTemp);
-            return OUT;
-        } else if (word.equals(STOP)) {
-            checkFlag = false;
-            dataTemp.add(STOP);
-            return word;
-        } else if (checkFlag) {
-            dataTemp.add(word);
-            answer = words.get(count);
-            dataTemp.add(answer);
-            count++;
-            return answer;
-        }
-        dataTemp.add(word);
-        return STOP;
-    }
-
-    public List<String> readFile(String path, Charset cs) throws IOException {
-        TreeSet<String> uniqueSortedWords = new TreeSet<String>();
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(path), cs))) {
-           String line = null;
-           while ((line = br.readLine()) != null) {
-               String[] words = line.split(" ");
-               for (String a : words) {
-                   uniqueSortedWords.add(a);
-               }
-           }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        List<String> list = new ArrayList<String>(uniqueSortedWords);
-        return list;
-    }
-
-    public void writeDataInFile(String path, List<String> data) {
-        try (PrintWriter pw = new PrintWriter(new FileWriter(path, Charset.forName("UTF-8"), true))) {
-            data.forEach(pw::println);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static void main(String[] args) throws IOException {
-        //String path = "/home/extrausa/IdeaProjects/job4j_design/test/textWord.txt"; // B
-        String path = "/home/denis/IdeaProjects/job4j_design/test/textWord.txt"; // A
-        //String botAnswers = "/home/extrausa/IdeaProjects/job4j_design/test/textAswer.txt";//B
-        String botAnswers = "/home/denis/IdeaProjects/job4j_design/test/textAswer.txt";//A
-        ConsoleChat cc = new ConsoleChat(path, botAnswers);
-        boolean fileinput = true;
         while (fileinput) {
-            Scanner in = new Scanner(System.in);
-            System.out.println("Введите слово");
-            String word = cc.run(in.next());
-            if (word.equals("стоп")) {
-                continue;
-            } else if (word.equals("закончить")) {
+            if (count == words.size()) {
+                count = 0;
+            }
+            if (word.equals(CONTINUE)) {
+                checkFlag = true;
+                dataTemp.add(word);
+                answer = words.get(count);
+                dataTemp.add(answer);
+                count++;
+                return answer;
+            }
+            if (word.equals(OUT)) {
+                dataTemp.add(OUT);
+                writeDataInFile(botAnswers, dataTemp);
                 fileinput = false;
-            } else {
-                System.out.println(word);
+                return OUT;
+            } else if (word.equals(STOP)) {
+                checkFlag = false;
+                dataTemp.add(STOP);
+                return word;
+            } else if (checkFlag) {
+                dataTemp.add(word);
+                answer = words.get(count);
+                dataTemp.add(answer);
+                count++;
+                return answer;
+            }
+            dataTemp.add(word);
+            return "введите слово";
+        }
+            //dataTemp.add(word);
+            return STOP;
+        }
+
+        public List<String> readFile (String path, Charset cs) throws IOException {
+            TreeSet<String> uniqueSortedWords = new TreeSet<String>();
+            try (BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(path), cs))) {
+                String line = null;
+                while ((line = br.readLine()) != null) {
+                    String[] words = line.split(" ");
+                    for (String a : words) {
+                        uniqueSortedWords.add(a);
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            List<String> list = new ArrayList<String>(uniqueSortedWords);
+            return list;
+        }
+
+        public void writeDataInFile (String path, List < String > data) {
+            try (PrintWriter pw = new PrintWriter(new FileWriter(path, Charset.forName("UTF-8"), true))) {
+                data.forEach(pw::println);
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
-    }
+
+        public static void main (String[]args) throws IOException {
+            //String path = "/home/extrausa/IdeaProjects/job4j_design/test/textWord.txt"; // B
+            String path = "/home/denis/IdeaProjects/job4j_design/test/textWord.txt"; // A
+            //String botAnswers = "/home/extrausa/IdeaProjects/job4j_design/test/textAswer.txt";//B
+            String botAnswers = "/home/denis/IdeaProjects/job4j_design/test/textAswer.txt";//A
+            ConsoleChat cc = new ConsoleChat(path, botAnswers);
+            System.out.println("Введите слово");
+            Scanner in = new Scanner(System.in);
+            boolean flagIn = true;
+            while (flagIn) {
+                String words = cc.run(in.next());
+                if (words.equals("закончить")) {
+                    flagIn = false;
+                    break;
+                }
+                System.out.println(words);
+
+            }
+
+        }
+
 }
 
 
