3de8bc3d2132199cf097a61dc4f7f9c8020d2036
Denis <d.kirenkov@gmail.com>
2021-09-03 16:04:28 +0300

    1. Бот [#7921]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -3,6 +3,7 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 //0. Что такое Socket? [#4850]00
+//1. Бот [#7921]
 public class EchoServer {
     public static void main(String[] args) throws IOException {
         try (ServerSocket server = new ServerSocket(9000)) {
@@ -14,11 +15,16 @@ public class EchoServer {
                     String word = in.readLine();
                     String[] wordArray = word.split(" ");
                     for (String answerWord : wordArray) {
-                        if (answerWord.endsWith("Bye")) {
+                        if (answerWord.endsWith("Hello")) {
+                            out.write("Hello".getBytes());
+                        } else if (answerWord.endsWith("Exit")) {
                             server.close();
+                        } else if (answerWord.endsWith("Any")) {
+                            out.write("What".getBytes());
                         }
                     }
-                    out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                    //out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                    //out.write("Hello, dear friend.".getBytes());
                     for (String str = in.readLine(); str != null && !str.isEmpty(); str = in.readLine()) {
                         System.out.println(str);
                     }
