5ee7033d7da09efe95cbdc4d34721217700204c8
Denis <d.kirenkov@gmail.com>
2021-09-10 14:46:36 +0300

    3. Формат XML [#313165 #200983]

---
+++ src/main/java/ru/job4j/io/json/Pet/InOutXML.java
@@ -0,0 +1,33 @@
+package ru.job4j.io.json.Pet;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import java.io.IOException;
+import java.io.StringWriter;
+
+public class InOutXML {
+    public static void main(String[] args) throws JAXBException {
+
+        final Pet pet = new Pet(true, 5, "Patrick",
+                new OwnerCard("Jack", "123-123"),
+                "Rick", "Mortilda");
+        // Получаем контекст для доступа к АПИ
+        JAXBContext context = JAXBContext.newInstance(Pet.class);
+        // Создаем сериализатор
+        Marshaller marshaller = context.createMarshaller();
+        // Указываем, что нам нужно форматирование
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+        String xml = "";
+        try (
+                StringWriter writer = new StringWriter()) {
+            // Сериализуем
+            marshaller.marshal(pet, writer);
+            xml = writer.getBuffer().toString();
+            System.out.println(xml);
+        } catch (
+                IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
