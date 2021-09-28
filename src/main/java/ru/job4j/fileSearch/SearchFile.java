ef1d978217988e7e19a06ab132f64270e370de34
Denis <d.kirenkov@gmail.com>
2021-09-28 07:08:34 +0300

    2. Поиск файлов по критерию [#783]02

---
+++ src/main/java/ru/job4j/fileSearch/SearchFile.java
@@ -0,0 +1,85 @@
+package ru.job4j.fileSearch;
+//2. Поиск файлов по критерию [#783]02
+
+import ru.job4j.io.ArgsName;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+import static ru.job4j.io.Search.search;
+
+public class SearchFile {
+    public static void main(String[] args) {
+        SearchFile searchFile = new SearchFile();
+        searchFile.validation(args);
+        ArgsName argsName = ArgsName.of(args);
+        Path file = Paths.get(argsName.get("d"));
+        String fullNameMatchFile = (argsName.get("n"));
+        String nameFile = (argsName.get("o"));
+        String typeFound = (argsName.get("t"));
+//        Path file = Paths.get("/home/denis/IdeaProjects/job4j_design");
+//        String fullNameMatchFile = (".*.js");
+//        String nameFile = ("log.txt");
+//        String typeFound = ("regex");
+        File path = new File("/home/denis/IdeaProjects/job4j_design/test", nameFile);
+
+        if (!path.exists()) {
+            try {
+                path.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        if (typeFound.equals("name")) {
+            try {
+                 List<Path> list = search(file, path1 -> path1.toFile().getName().equals(fullNameMatchFile));
+                if (!list.isEmpty()) {
+                    for (Path p : list) {
+                        searchFile.writeFile(path, p.toFile());
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else if (typeFound.equals("mask")) {
+            try {
+                List<Path> list = search(file, path1 -> path1.toFile().getName().endsWith(fullNameMatchFile));
+                if (!list.isEmpty()) {
+                    for (Path p : list) {
+                        searchFile.writeFile(path, p.toFile());
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else if (typeFound.equals("regex")) {
+            try {
+                List<Path> list = search(file, path1 -> path1.toFile().getName().matches(fullNameMatchFile));
+                if (!list.isEmpty()) {
+                    for (Path p : list) {
+                        searchFile.writeFile(path, p.toFile());
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void validation(String[] args) {
+        if (args.length != 4) {
+            throw new IllegalArgumentException("Root folder is null. Usage java -jar dir.jar ROOT_FOLDER.");
+        }
+    }
+
+    private void writeFile(File directoryLog, File subfile) {
+        byte[] bufAbsolutAdress = (subfile.getAbsolutePath() + System.lineSeparator()).getBytes(StandardCharsets.UTF_8);
+        try (FileOutputStream file = new FileOutputStream(directoryLog, true)) {
+                file.write(bufAbsolutAdress);
+        } catch (IOException e) {
+                e.printStackTrace();
+        }
+    }
+}
