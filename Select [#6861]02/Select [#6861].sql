fab9a58863e6645670229fde9ff64af34cfc521a
Denis <d.kirenkov@gmail.com>
2021-10-10 20:20:14 +0300

    select

--- Select [#6861]02/Select [#6861].sql
+++ Select [#6861]02/Select [#6861].sql
@@ -5,17 +5,16 @@ CREATE TABLE fauna (
     discovery_date DATE
     );
 
-INSERT INTO fauna(name, avg_age, discovery_date) VALUES('fish', 5, TO_DATE('17/12/2015', 'DD/MM/YYYY'));
-INSERT INTO fauna(name, avg_age, discovery_date) VALUES('flish', 5, TO_DATE('17/12/2015', 'DD/MM/YYYY'));
-INSERT INTO fauna(name, avg_age, discovery_date) VALUES('flosh', 5, TO_DATE('17/12/2015', 'DD/MM/YYYY'));
-INSERT INTO fauna(name, avg_age, discovery_date) VALUES('fish_pig', 5, TO_DATE('17/12/1949', 'DD/MM/YYYY'));
+
+INSERT INTO fauna(name, avg_age, discovery_date) VALUES('fish', 5, '17-12-2015');
+INSERT INTO fauna(name, avg_age, discovery_date) VALUES('fish_pig', 5, '17-12-1949');
 INSERT INTO fauna(name, avg_age, discovery_date) VALUES('elephan', 5, null);
-INSERT INTO fauna(name, avg_age, discovery_date) VALUES('crocodile', 5, TO_DATE('17/12/1814', 'DD/MM/YYYY'));
-INSERT INTO fauna(name, avg_age, discovery_date) VALUES('leon', 5, TO_DATE('17/12/1965', 'DD/MM/YYYY'));
-INSERT INTO fauna(name, avg_age, discovery_date) VALUES('monkey', 5, TO_DATE('17/12/1950', 'DD/MM/YYYY'));
+INSERT INTO fauna(name, avg_age, discovery_date) VALUES('crocodile', 5, '17-12-1814');
+INSERT INTO fauna(name, avg_age, discovery_date) VALUES('leon', 5, '17-12-1965');
+INSERT INTO fauna(name, avg_age, discovery_date) VALUES('monkey', 5, '17/12/1950');
 
 SELECT * FROM fauna WHERE name LIKE '%fish%';
-SELECT * FROM fauna WHERE avg_age > 0 AND avg_age < 10;
+SELECT * FROM fauna WHERE avg_age > 0 BETWEEN avg_age < 10;
 SELECT * FROM fauna WHERE discovery_date IS null;
 SELECT * FROM fauna WHERE discovery_date <'01.01.1950';
 
