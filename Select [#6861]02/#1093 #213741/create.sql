9edf0c7ea3e1a27942c0bafb1a96477a14e7e2f1
Denis <d.kirenkov@gmail.com>
2021-10-10 20:45:04 +0300

    Схема прав пользователи и роли. [#1093 #213741]

---
+++ Select [#6861]02/#1093 #213741/create.sql
@@ -0,0 +1,18 @@
+CREATE TABLE user_role (id_role SERIAL PRIMARY KEY, role_name TEXT);
+CREATE TABLE users(user_id SERIAL PRIMARY KEY, user_role INT REFERENCES user_role(id_role), name TEXT);
+CREATE TABLE role_rules (id_rules SERIAL PRIMARY KEY, rules_name TEXT);
+CREATE TABLE group_role_rules (id SERIAL PRIMARY KEY,
+                            id_role INT REFERENCES user_role(id_role),
+                            id_rules INT REFERENCES role_rules(id_rules));
+
+
+CREATE TABLE category (id_category SERIAL PRIMARY KEY, name TEXT);
+CREATE TABLE state (id_state SERIAL PRIMARY KEY, name TEXT);
+CREATE TABLE item (id_item SERIAL PRIMARY KEY,
+                user_id INT REFERENCES users(user_id),
+                id_category INT REFERENCES category(id_category),
+                id_state INT REFERENCES state(id_state),
+                cost INT);
+CREATE TABLE comments (id_comments SERIAL PRIMARY KEY, id_item INT REFERENCES item(id_item), name TEXT);
+CREATE TABLE attaches (id_attaches SERIAL PRIMARY KEY, id_item INT REFERENCES item(id_item), name TEXT);
+
